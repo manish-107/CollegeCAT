04103cd8c177b0657f20adc5650e5bae3ee55443
manish-107 <creativecrew2676@gmail.com>
2025-02-09 21:14:36 +0530

    added nextjs and login button

---
+++ collegecatapp/components/ui/buttons/GoogleLoginButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import GoogleIcon from "../icons/GoogleIcon";
+
+const GoogleLoginButton = () => {
+  const justFunction = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div
+      onClick={justFunction}
+      className="flex gap-32 bg-[#403f3f6d] p-4 rounded-lg border border-[#aaaaaa4a] hover:bg-[#63616159] hover:cursor-pointer"
+    >
+      <div className="flex gap-4">
+        <GoogleIcon />
+        <button className="rounded-md font-normal" type="button">
+          Login with goggle
+        </button>
+      </div>
+      <span className="text-[#afadad]"> {` ->`}</span>
+    </div>
+  );
+};
+
+export default GoogleLoginButton;
